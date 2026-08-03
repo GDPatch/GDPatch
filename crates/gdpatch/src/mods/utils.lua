68b97021d836ddfb4850e1b9979ddcb8fe269673
NotNite <hi@notnite.com>
2026-08-02 22:43:24 -0400

    lua: Add percent_only argument to utils.escape
    You still need to escape % in the return value of string.gsub, which is
    quite annoying...

--- crates/gdpatch/src/mods/utils.lua
+++ crates/gdpatch/src/mods/utils.lua
@@ -1,6 +1,11 @@
 local utils = {}
 
-function utils.escape(str)
+function utils.escape(str, percent_only)
+  -- the second argument to gsub (repl) only needs to escape percentages
+  if percent_only then
+    return (str:gsub("%%", "%%%%"))
+  end
+
   -- https://github.com/lua-nucleo/lua-nucleo/blob/4f30d4178c31417e9df9b976a0f36b3157c3e3b5/lua-nucleo/string.lua#L245-L267
   return (str:gsub(".", {
     ["^"] = "%^",
